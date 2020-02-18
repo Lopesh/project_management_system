46534f46cc53055c858a2b80544cd5465215791e
Lopesh <durugkarlopesh@gmail.com>
2020-02-18 21:01:57 +0530

    - Added CSS for the Login Form. - Implement Device to the User Model. - Migrations for the User Device Model. - Added User Controller and Projects controller. - Added Index pages for projects.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,12 +5,43 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-
-10.times do |user|
-  user = User.new({
-                         first_name: Faker::Name.first_name,
-                         last_name: Faker::Name.last_name,
-                         role: rand(1..3)
-                        })
-  user.save
+if User.all.empty?
+  20.times do |user|
+    user = User.new({
+                        first_name: Faker::Name.first_name,
+                        last_name: Faker::Name.last_name,
+                        role: rand(1..3)
+                    })
+    user.save
+  end
 end
+
+
+if Project.all.empty?
+  @admin_array = User.where(role: User.roles['Admin']).pluck(:id)
+  10.times do |user|
+    project = Project.new({
+                              name: Faker::Job.title,
+                              description: Faker::Job.field,
+                              created_by_id: @admin_array.sample,
+                              updated_by_id: @admin_array.sample
+                          })
+    project.save
+  end
+end
+
+if Task.all.empty?
+  @admin_array = User.where(role: User.roles['Admin']).or(User.where(role: User.roles['Project Manager'])).pluck(:id)
+  @projects = Project.all.pluck(:id)
+  100.times do |user|
+    task = Task.new({
+                              name: Faker::Job.title,
+                              description: Faker::Job.field,
+                              created_by_id: @admin_array.sample,
+                              updated_by_id: @admin_array.sample,
+                              project_id: @projects.sample
+                          })
+    task.save
+  end
+end
+
